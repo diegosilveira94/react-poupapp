ef3172c3da701d6f3b19e4cfa0e5c39dc28bda97
Diego Silveira <diego.silveira@ngi.com.br>
2026-02-16 18:58:35 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <>
+      <aside>
+        <img src={logo} alt="logo" />
+        <h1>PoupApp</h1>
+      </aside>
+    </>
+  );
+}
+
+export default App;
